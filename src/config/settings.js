cf00a76bb2621cebf4aadf5c52423c312c2f376f
vivan123456789 <vivanvaidya@icloud.com>
2026-05-25 22:12:58 -0700

    Disable theme switcher in settings

--- src/config/settings.js
+++ src/config/settings.js
@@ -3,6 +3,6 @@ export default {
 	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
 	url: `https://odyssey-theme.sapling.supply`, // No trailing slash!
 	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
-	enableThemeSwitcher: true,
+	enableThemeSwitcher: false,
 	showPlug: true, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
 };
